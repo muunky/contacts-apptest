3883c0b571ede1ac472c1f89de1dd318e27b42cc
maigre-ai <thomas@37m.gr>
2024-10-23 14:08:44 +0200

    init commit

---
+++ server.js
@@ -0,0 +1,31 @@
+// Express + socketio server
+
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'www')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'www/index.html'));
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+})
+
+server.listen(3007, () => {
+    console.log('listening on *:3007');
+})
---
+++ www/bootstrap.js
@@ -0,0 +1,6 @@
+// dynamically load and execute the launcher.js script
+
+var script = document.createElement("script");
+script.src = "launcher.js";
+document.head.appendChild(script);
+
