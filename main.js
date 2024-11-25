2736cfe8c40af13a3fb222e31dd0a13c5f4e215d
muunky <jusdcitron@gmail.com>
2024-11-25 10:17:55 +0100

    test

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html')); // Adjust the path as necessary
+  res.sendFile(path.join(__dirname, 'www/index.html')); // Adjust the path as necessary
 });
 
 // route de check for update
